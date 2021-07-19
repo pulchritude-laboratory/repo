c6ba95952b26254da0e17195599de9b076f79505
Bankuti Adam <bankuti.adam@grape.hu>
2021-07-19 17:20:10 +0200

    added a new test for the button component

--- libs/ui-antd/src/lib/button/Button.spec.tsx
+++ libs/ui-antd/src/lib/button/Button.spec.tsx
@@ -1,10 +1,15 @@
-import { render } from '@testing-library/react';
+import { render } from '@testing-library/react'
 
-import Button from './Button';
+import Button from './Button'
 
 describe('Button', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Button />);
-    expect(baseElement).toBeTruthy();
-  });
-});
+    const { baseElement } = render(<Button />)
+    expect(baseElement).toBeTruthy()
+  })
+
+  it('should render the title', () => {
+    const { baseElement } = render(<Button title="TEXT" />)
+    expect(baseElement.textContent).toBe('TEXT')
+  })
+})
