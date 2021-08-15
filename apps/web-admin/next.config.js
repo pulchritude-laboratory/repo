3f263b522c487850990e9dff85f480cf03e6ab4c
Bankuti Adam <bankuti.adam@grape.hu>
2021-08-15 21:45:41 +0200

    added basepath

--- apps/web-admin/next.config.js
+++ apps/web-admin/next.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNx = require('@nrwl/next/plugins/with-nx');
+const withNx = require('@nrwl/next/plugins/with-nx')
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -8,8 +8,9 @@ const nextConfig = {
   nx: {
     // Set this to false if you do not want to use SVGR
     // See: https://github.com/gregberge/svgr
-    svgr: true,
+    svgr: true
   },
-};
+  basePath: '/admin'
+}
 
-module.exports = withNx(nextConfig);
+module.exports = withNx(nextConfig)
